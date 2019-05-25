f717587691a0df816c308189ce2e4f64444be33f
Jrad84 <jaredtaback@gmail.com>
2019-05-25 16:10:57 +1000

    cleaned up python code

--- Pi/test.py
+++ Pi/test.py
@@ -7,57 +7,53 @@ import serial
 import time
 import pymysql
 import pymysql.cursors
-import sqlite3
+import sqlite3 as sql
 import datetime
 
 
 app = Flask(__name__)
 #device = "COM7"
-device = "/dev/ttyUSB1"
+deviceA = "/dev/ttyUSB0"
+deviceB = "/dev/ttyUSB1"
 # open the serial port to talk over
-s1 = serial.Serial(device, 9600)
+s1 = serial.Serial(deviceA, 9600)
+s2 = serial.Serial(deviceB, 9600)
 
 # clean the serial port
 s1.flushInput()
-
-class Arduino:
-  def __init__(self):
-    self.distance = None
-    self.lightStatus = None
-    self.threshold = 30
-    
-  def read(self):
-    # make sure there is some data to read
-    if s1.inWaiting():
-      while s1.inWaiting():
-          self.distance = s1.readline()
-          self.lightStatus = s1.readline()
-          self.threshold = s1.readline()
-          
-    conn = getDBConnection()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO INCIDENTS (distance, date) VALUES (?, ?)", (self.distance, datetime.datetime.now()))
-    conn.commit()
-    #data = cur.execute("SELECT distance, date FROM INCIDENTS")
-    #if (not data):
-    #    print("Error")
-    #for row in data:
-    #    print ("Distance : ", row[0])
-    #    print ("Date : ", row[1], "\n")
-    #conn.close()
-        #print(self.distance, self.lightStatus, self.threshold)
-    return self.distance, self.lightStatus, self.threshold
-
-ard = Arduino()
-#data = ard.read()
-#print(data)
-
 
 def getDBConnection():
     #conn = sql.connect('database.db')
     conn = sqlite3.connect('test.db')
     conn.execute('CREATE TABLE if not exists INCIDENTS (distance REAL, date TEXT)')
     return conn
+
+class Arduino:
+  def __init__(self):
+    self.distance = None
+    self.lightStatus = None
+    self.threshold = 60.0
+    
+  def read(self):
+    # make sure there is some data to read
+    #if s1.inWaiting():
+     # while s1.inWaiting():
+    self.distance = s1.readline()
+    self.lightStatus = s1.readline()
+    self.threshold = s1.readline()
+    return self.distance, self.lightStatus, self.threshold
+    conn = getDBConnection()
+    cur = conn.cursor()
+    if (self.distance < 60.0):
+        cur.execute("INSERT INTO INCIDENTS (distance, date) VALUES (?, ?)", (self.distance, datetime.datetime.now()))
+        conn.commit()
+    
+ard = Arduino()
+data = ard.read()
+print(data)
+
+
+
 
 # route used by Flask to show the main page
 @app.route('/', methods=['GET', 'POST'])
@@ -66,9 +62,14 @@ def index():
   # collect all information to show to user
   title = 'Smart Light Controller'
   arduinoDistance, arduinoLightStatus, arduinoThreshold = ard.read()
+  conn = getDBConnection()
+  conn.row_factory = sql.Row
+  cur = conn.cursor()
+  cur.execute("select * from Incidents")
+  rows = cur.fetchall();
   #formTemp.tempTreshold.data = arduinoThreshold
   # return the webpage and pass it all of the information
-  return render_template( 'index.html', title=title, arduinoDistance=arduinoDistance, arduinoLightStatus=arduinoLightStatus)
+  return render_template( 'index.html', title=title, arduinoDistance=arduinoDistance, arduinoLightStatus=arduinoLightStatus, rows=rows)
 
 @app.route('/list')
 def list():
